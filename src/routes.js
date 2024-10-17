8917105022787ccbc7f2874c5ea748a7d90f9a17
31032019 <jpcostac17@gmail.com>
2024-10-17 01:53:52 -0300

    Completed all routes for the API implementation

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const route = express.Router()
 const cors = require('cors')
 const ClienteController = require('./controllers/ClienteController');
 const BarbeiroController = require('./controllers/BarbeiroController');
+const AgendamentoController = require('./controllers/AgendamentoController');
+const AtendenteController = require('./controllers/AtendenteController');
+const ServicoController = require('./controllers/ServicoController');
 
 
 
@@ -16,6 +19,24 @@ route.get('/barbeiro', BarbeiroController.showBarbeiro);
 route.post('/barbeiro', BarbeiroController.createBarbeiro);
 route.update('/barbeiro/:id', BarbeiroController.updateBarbeiro);
 route.delete('/barbeiro/:id', BarbeiroController.deleteBarbeiro);
+
+//endPoints agendamentos
+route.get('/agendamento', AgendamentoController.showAgendamentos);
+route.post('/agendamento', AgendamentoController.createAgendamento);
+route.update('/agendamento/:id', AgendamentoController.updateAgendamento);
+route.delete('/agendamento/:id', AgendamentoController.deleteAgendamento);
+
+//endPoints atendentes
+route.get('/atendente', AtendenteController.showAtendentes);
+route.post('/atendente', AtendenteController.createAtendente);
+route.update('/atendente/:id', AtendenteController.updateAtendente);
+route.delete('/atendente/:id', AtendenteController.deleteAtendente);
+
+//endPoints serciços
+route.get('/servicos', ServicoController.showServicos);
+route.post('/servicos', ServicoController.createServico);
+route.update('/servicos/:id', ServicoController.updateServico);
+route.delete('/servicos/:id', ServicoController.deleteServico);
 
 
 module.exports = route
